2e498b475232b9c73fea468d9d1c9772c34bcf47
ciaaudi <146406439+ciaaudi@users.noreply.github.com>
2024-11-19 19:45:05 +0700

    10.02, 10.03, 10.04

--- app/src/main/java/com/example/recyclerview_c14220017/wayang.kt
+++ app/src/main/java/com/example/recyclerview_c14220017/wayang.kt
@@ -1,8 +1,13 @@
 package com.example.recyclerview_c14220017
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+
+@Parcelize
 data class wayang(
     var foto : String,
     var nama : String,
     var karakter : String,
     var deskripsi : String
-)
+): Parcelable
